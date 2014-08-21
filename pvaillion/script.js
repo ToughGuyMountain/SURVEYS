765dcb54bebdf369c2654799c884e3e8017361e9
Jonathan Carroll <jonathaniscarroll@gmail.com>
2014-08-21 18:57:08 -0400

    finished the title page made moving animation

--- pvaillion/script.js
+++ pvaillion/script.js
@@ -4,6 +4,25 @@ $(document).ready(function(){
 	
 	$("#main").height(height);
 
-	$("#content").height(height - 200);
-	$("#content").width(width - 200);
+	$("#content").height("75%");
+	$("#content").width("75%");
+
+	moveMug1();
+
+	function moveMug1(){
+		var top = Math.floor((Math.random() * 100 - 50) + 1);
+		var left = Math.floor((Math.random() * 100 - 50) + 1);
+		$("#mug1").animate({
+			top: "+="+top,
+			left: "+="+left
+		},1000);
+		$("#mug2").animate({
+			top:"-="+top,
+			left:"-="+left},
+			1000);
+		moveMug1();
+	}
+
+
+
 });
