42ad0581eea236ff8c8b8eb0f5ad5dfd84b042e5
Jonathan Carroll <jonathaniscarroll@gmail.com>
2014-08-27 12:25:13 -0400

    adding new slides

--- pvaillion/script.js
+++ pvaillion/script.js
@@ -7,22 +7,6 @@ $(document).ready(function(){
 	$(".content").height("75%");
 	$(".content").width("75%");
 
-	moveMug1();
-
-	function moveMug1(){
-		var top = Math.floor((Math.random() * 100 - 50) + 1);
-		var left = Math.floor((Math.random() * 100 - 50) + 1);
-		$("#mug1").animate({
-			top: "+="+top,
-			left: "+="+left
-		},1000);
-		$("#mug2").animate({
-			top:"-="+top,
-			left:"-="+left},
-			1000);
-		moveMug1();
-	}
-
 
 
 });
